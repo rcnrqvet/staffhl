12711c87197bdd5595ccde2fb18dc4859736d6b1
rcn <sigmarust3@gmail.com>
2026-01-21 19:27:10 -0500

    Add files via upload

---
+++ staffhl-github/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'rcnrqvet'
+description 'Staff Highlighting System'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_script 'client/cl_main.lua'
+
+server_script 'server/sv_main.lua'
